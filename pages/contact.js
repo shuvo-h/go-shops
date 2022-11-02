a7b69bafba3807c3f7fbe36bb057ff38de3d5676
shuvo-h <shuvoh38@gmail.com>
2022-11-02 17:29:52 +0600

    clearing git track

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+const Contact = () => {
+    
+    return (
+        <div>
+            Contact us
+        </div>
+    );
+};
+
+export default Contact;
